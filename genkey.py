c0fbcca8e6174f28ebe5a14b0f055d0ca4a0e824
sun-0806 <95276015+sun-0806@users.noreply.github.com>
2024-11-26 19:21:00 +0800

    Add files via upload

---
+++ genkey.py
@@ -0,0 +1,36 @@
+import secrets
+
+import numpy as np
+
+
+def Keygen(m):
+    """
+    # 输入参数m，返回密钥sk
+    :param m: 随机矩阵的维度
+    :return: sk=(M1,M2,S)
+    """
+    m1 = generate_invertible_matrix(m)
+    m2 = generate_invertible_matrix(m)
+    S = np.random.randint(0, 2, [m])
+    M1 = np.matrix(m1)
+    M2 = np.matrix(m2)
+
+    sk = [np.transpose(M1), np.transpose(M2), np.linalg.inv(M1), np.linalg.inv(M2), S]
+    return sk
+
+
+def Hash_keys(hash_count):
+    K = []
+    for i in range(hash_count):
+        K.append(secrets.token_hex(16).encode())
+    return K
+
+
+def generate_invertible_matrix(size):
+    while True:
+        matrix = np.random.rand(size, size)
+
+        det = np.linalg.det(matrix)
+
+        if det != 0:
+            return matrix
