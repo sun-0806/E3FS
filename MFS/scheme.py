6412d7d0b2bbad15b766c5a2fe422594ea3155ad
sun <special0806@ustc.edu.cn>
2024-12-04 20:51:04 +0800

    update

--- MFS/scheme.py
+++ MFS/scheme.py
@@ -1,4 +1,3 @@
-import logging
 import time
 
 import numpy as np
@@ -8,18 +7,10 @@ import random
 from AuthTree import ASPE
 from BF_plain import file_to_BF, query_to_BF
 from fuzzision import Read_file, Preprocess, Vect_files, gen_e2LSH_family, biGram_dictionary
-from Genkey import Keygen1
-
-
-'''
-包括密钥生成，搜索验证协议各部分
-'''
+from Genkey import Keygen
 
 
 def indexGen(P, gram_dict, m, file_num, dic_size, sk):
-    '''
-    索引创建
-    '''
     Dmap = {}
     BFs = file_to_BF(P, gram_dict, m, file_num, dic_size)
     for i in range(len(BFs)):
@@ -28,7 +19,7 @@ def indexGen(P, gram_dict, m, file_num, dic_size, sk):
     return Dmap
 
 
-def GenTrap(Qv, sk, BF_len):  # tag:从gram_dict中随机选取若干个
+def GenTrap(Qv, sk, BF_len):  
     BF = query_to_BF(Qv, BF_len)
     logger.info(f"query BF:{BF.bit_array}")
     sco = BF.one
@@ -46,56 +37,36 @@ def search(sco, trap, Dmap):
 
 
 def is_match(BF, trap1, sco):
-    """
-    判断是否满足匹配条件，返回一个flag和相似度得分
-    """
     sco1 = np.inner(BF, trap1)
     sco1 = np.array(sco1).flatten()[0]
-    logger.info(f"预期sco:{sco}, 实际sco:{sco1}")
     if math.fabs(sco - sco1) <= 0.1:
         return 1
     return 0
 
 
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
 
-logger.info("Start print log")
-
-file_dir = "E:/data/medtest1/medtest/medtest500"
+file_dir = "./medata"
 Flist, FHlist = Read_file(file_dir)
 Flist = Preprocess(Flist)
-dictionary, P = Vect_files(Flist, 800)  # 200是提取关键词总数
-file_num, dic_size = P.shape  # dic_size是所有文件关键词提取个数
-logger.info("file processing over")
+dictionary, P = Vect_files(Flist, 4000)  
+file_num, dic_size = P.shape  
 m = 1200
-sk = Keygen1(m)
+sk = Keygen(m)
 
 N_uni = 676
-Num_LSH = 8
-c = 3  # LSH的分母
+Num_LSH = 6
+c = 3  
 e2LSH_family = gen_e2LSH_family(N_uni, Num_LSH, c)
 gram_set = biGram_dictionary(e2LSH_family, dictionary, c)
-logger.info("indexGen start")
-time_start1 = time.time() #开始计时
 Dmap = indexGen(P, gram_set, m, file_num, dic_size, sk)
-time_end1 = time.time()    #结束计时
-logger.info(f"indexGen over:{time_end1-time_start1}")
+time_end1 = time.time()    
 query_keywords = []
-obj = random.sample(range(800), 5)
-for i in range(5):
+obj = random.sample(range(800), 1)
+for i in range(1):
     query_keywords.append(dictionary[obj[i]])
 Qv = biGram_dictionary(e2LSH_family, query_keywords, c)
-logger.info("GenTrap start")
-time_start2 = time.time() #开始计时
 traps, sco = GenTrap(Qv, sk, m)
-time_end2 = time.time()    #结束计时
-logger.info(f"GenTrap over:{time_end2-time_start2}")
-
-logger.info("search start")
-time_start3 = time.time()  # 开始计时
 R_i= search(sco, traps, Dmap)
-time_end3 = time.time()  # 结束计时
-logger.info(f"search over:{time_end3 - time_start3}")
+time_end3 = time.time()  
 print('match_ids=', R_i)
--- README.md
+++ README.md
@@ -1,3 +1,33 @@
-# E^3FS
+# E^3FS: Efficient, Secure, and Verifiable Fuzzy Search with Data Updates in Hybrid-Storage Blockchains
 
- Run scheme.py to implement a verifiable secure fuzzy multi-keyword search.
+## Artifact Dependencies and Requirements
+**Hardware resources required:** An x64 system with 16GB free memory.
+**Operating systems required:** Windows/Linux.
+**Software libraries needed:** Python, NumPy, pypbc, sklearn, matplotlib, etc.
+
+## Reproducibility of Experiments
+**Algorithms Implemented in This Package:**
+1. **E^3FS:** Our proposed algorithm for Hybrid-Storage blockchains.
+2. **VFSA:** A baseline algorithm for encrypted databases from [Li 23](https://ieeexplore.ieee.org/abstract/document/9669122).
+3. **VRMFS:** A baseline algorithm for encrypted databases from [Tong 23](https://ieeexplore.ieee.org/document/9714876).
+4. **MFS:** A baseline algorithm for encrypted databases from [Wang 14](https://ieeexplore.ieee.org/document/6848153).
+
+I've simply adjusted the formatting to make the algorithm names and descriptions stand out more clearly.
+
+## Complete Description of Packages
+
+**dataset**
+A real-world medical dialogue dataset from [Chen 20](https://arxiv.org/pdf/2004.03329v1).
+
+**E3FS**
+
+Contains the following programs:
+
+- `Genkey.py`: Program to generate the secret keys for the scheme.
+- `fuzzy_process.py`: Program to read files and create fuzzy keywords.
+- `BF_process.py`: Program to create a forward index for each file.
+- `ABBT.py`: Program to create an A-BBT for each keyword.
+- `scheme.py`: Program to implementate an E3FS scheme.
+
+The algorithm implementation of the baseline scheme can be found in each algorithm package, such as `VFSA`, `VRMFS`, and `MFS`. All protocols can be directly executed by running `scheme.py`.
+
