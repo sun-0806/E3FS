e030fdbb5a0885a91417f1af7c4ed670dc0a7f0d
sun-0806 <95276015+sun-0806@users.noreply.github.com>
2024-12-04 21:34:09 +0800

    Update

--- MFS/scheme.py
+++ MFS/scheme.py
@@ -1,3 +1,5 @@
+import logging
+import sys
 import time
 
 import numpy as np
@@ -5,22 +7,25 @@ import math
 import random
 
 from AuthTree import ASPE
-from BF_plain import file_to_BF, query_to_BF
+from BF_plain import file_to_BF, query_to_BF, HMAC_keys
 from fuzzision import Read_file, Preprocess, Vect_files, gen_e2LSH_family, biGram_dictionary
 from Genkey import Keygen
 
 
-def indexGen(P, gram_dict, m, file_num, dic_size, sk):
+
+def indexGen(P, gram_dict, m, file_num, dic_size, sk, K, Num_HMAC):
     Dmap = {}
-    BFs = file_to_BF(P, gram_dict, m, file_num, dic_size)
+    BFs = file_to_BF(P, gram_dict, m, file_num, dic_size, K, Num_HMAC)
+    temp = 0
     for i in range(len(BFs)):
         EBF = ASPE(BFs[i].bit_array, sk, True)
-        Dmap[i]= EBF
-    return Dmap
+        Dmap[i] = EBF
+        temp += sys.getsizeof(EBF)
+    return Dmap,temp
 
 
-def GenTrap(Qv, sk, BF_len):  
-    BF = query_to_BF(Qv, BF_len)
+def GenTrap(Qv, sk, BF_len, K, Num_HMAC): 
+    BF = query_to_BF(Qv, BF_len, K, Num_HMAC)
     logger.info(f"query BF:{BF.bit_array}")
     sco = BF.one
     trapdoor = ASPE(BF.bit_array, sk, False)
@@ -44,29 +49,37 @@ def is_match(BF, trap1, sco):
     return 0
 
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
+logger.info("Start print log")
 
 file_dir = "./medata"
 Flist, FHlist = Read_file(file_dir)
 Flist = Preprocess(Flist)
-dictionary, P = Vect_files(Flist, 4000)  
-file_num, dic_size = P.shape  
+dictionary, X_tfidf = Vect_files(Flist, 4000) 
+P = X_tfidf.toarray()
+file_num, dic_size = P.shape 
 m = 1200
 sk = Keygen(m)
 
 N_uni = 676
 Num_LSH = 6
-c = 3  
+c = 3  # LSH的分母
+
+Num_Hmac = 10
+K = HMAC_keys(Num_Hmac)
+
 e2LSH_family = gen_e2LSH_family(N_uni, Num_LSH, c)
 gram_set = biGram_dictionary(e2LSH_family, dictionary, c)
-Dmap = indexGen(P, gram_set, m, file_num, dic_size, sk)
-time_end1 = time.time()    
+
+
 query_keywords = []
-obj = random.sample(range(800), 1)
-for i in range(1):
+obj = random.sample(range(800), 5)
+for i in range(5):
     query_keywords.append(dictionary[obj[i]])
 Qv = biGram_dictionary(e2LSH_family, query_keywords, c)
-traps, sco = GenTrap(Qv, sk, m)
-R_i= search(sco, traps, Dmap)
-time_end3 = time.time()  
+traps, sco = GenTrap(Qv, sk, m, K, Num_Hmac)
+R_i = search(sco, traps, Dmap)
 print('match_ids=', R_i)
+
