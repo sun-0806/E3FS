266fd2e6e8e565fb6c1ad588a60dea7cd811c0f5
sun-0806 <95276015+sun-0806@users.noreply.github.com>
2025-02-13 23:35:51 +0800

    Update BF_plain.py

--- VRMFS/BF_plain.py
+++ VRMFS/BF_plain.py
@@ -6,6 +6,11 @@ import numpy as np
 
 
 def HMAC_keys(hash_count):
+    '''
+    PRF Key Generation.
+    :param hash_count: the number of hash functions
+    :return: PRF keys
+    '''
     K = []
     for i in range(hash_count):
         K.append(secrets.token_hex(16).encode())  
@@ -13,14 +18,27 @@ def HMAC_keys(hash_count):
 
 
 class BF_plain:
+     '''
+    Definition of Bloom Filter.
+    '''
 
     def __init__(self, hash_count, m):  
+        '''
+        Initialize Bloom filter
+        :param hash_count: the number of hash functions
+        :param m: The length of Bloom Filter
+        '''
         self._size = m
         self._hash_count = hash_count
         self.bit_array = np.zeros(m, dtype=int)
         self.one = 0
 
     def insert(self, obj, K):
+        '''
+        The insertion of Bloom filter
+        :param obj: Insert Object
+        :param K: PRF keys
+        '''
         for i in range(self._hash_count):
             a = hmac.new(K[i], obj.encode(), digestmod=hashlib.sha1).hexdigest()
             hash_value = int(a, 16) % math.ceil(self.size)
@@ -34,6 +52,17 @@ class BF_plain:
 
 
 def file_to_BF(P, gram_dict, BF_len, file_num, dic_size, K, Num_HMAC):
+    '''
+    Process the file set, generate the Bloom filter index of the files.
+    :param P: TF-IDF matrix representation of file list
+    :param gram_dict: the unigram dictionary of keywords
+    :param Num_Hmac: the number of hash functions
+    :param BF_len: the length of Bloom Filter
+    :param file_num: the number of files
+    :param dic_size: the number of keywords
+    :param K: PRF keys
+    :return: the Bloom filter list of the file set
+    '''
     BFs = []
     for i in range(file_num):
         BF = BF_plain(Num_HMAC,BF_len) 
@@ -45,6 +74,14 @@ def file_to_BF(P, gram_dict, BF_len, file_num, dic_size, K, Num_HMAC):
 
 
 def query_to_BF(Qv, BF_len, K, Num_HMAC):
+    '''
+    Process the query, generate the Bloom filter for the query.
+    :param Qv: the unigram dictionary of the query
+    :param Num_Hmac: the number of hash functions
+    :param BF_len: the length of Bloom Filter
+    :param K: PRF keys
+    :return: the Bloom filter of the query
+    '''
     BF = BF_plain(Num_HMAC, BF_len)  
     for i in range(len(Qv)):
         BF.insert(Qv[i],K)  
