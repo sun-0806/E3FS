424815fe8e14ea28189f2a7db3bdd3037fdbd3e3
sun-0806 <95276015+sun-0806@users.noreply.github.com>
2025-02-14 00:05:07 +0800

    Update MulAcc.py

--- VFSA/MulAcc.py
+++ VFSA/MulAcc.py
@@ -3,13 +3,17 @@ import numpy as np
 from poly import poly_self,subtract_poly,multiply_poly,add_poly,divide_poly
 
 def Acc(P,pk,pairing):
-    acc=Element.one(pairing, G2)
+    acc=Element.one(pairing, G2) #Initializes an element in G2 to 1
     for i in range(len(P)):
         v=Element(pairing, G2, value =pk[i]**P[i])
         acc=Element(pairing, G2, value = acc*v)
     return acc
 
 def ext_euclid(f, h,pairing):
+    '''
+    The extended Euclidean algorithm finds x,y such that xf+yh=gcd(f,h)
+    The highest degree of f is greater than or equal to h
+    '''
     if (len(h) == 1 and h[0]==Element.zero( pairing, Zr)) or len(h) == 0:
         return [Element.one(pairing, Zr)], [Element.zero( pairing, Zr)], f
     else:
@@ -19,6 +23,7 @@ def ext_euclid(f, h,pairing):
         return x, y, coef
 
 def Proved_disjoint(P1_Zr,P2_Zr,pk,pairing):
+    '''Find the polynomial Q1,Q2 satisfies P1_Zr*Q1+P2_Zr*Q2=gcd(P1_Zr,P2_Zr)'''
     Q1,Q2,coef=ext_euclid(P1_Zr,P2_Zr,pairing)
     #print('Q1,Q2,coef=',Q1,Q2,coef)
     if len(coef) != 1:
@@ -30,6 +35,7 @@ def Proved_disjoint(P1_Zr,P2_Zr,pk,pairing):
 
 
 def Verify_disjoint(acc1, acc2, pi_1, pi_2, coef, pairing,g):
+     '''Multi-set accumulator verification algorithm'''
     right = Element(pairing, GT, value=pairing.apply(g,g) ** coef)
     left = Element(pairing, GT, value=pairing.apply(acc1, pi_1) * pairing.apply(acc2, pi_2))
     if left == right:
@@ -51,6 +57,6 @@ def keyGen(m):
     return g, pk, pairing
 
 def process(X, pairing):
-    P = poly_self(list(np.array(X)))
-    P_Zr = [Element(pairing, Zr, value=int(item)) for item in P]
+    P = poly_self(list(np.array(X))) # Given the root X of the polynomial, find the coefficient P
+    P_Zr = [Element(pairing, Zr, value=int(item)) for item in P] # Convert coefficients to elements in Zr
     return P_Zr
