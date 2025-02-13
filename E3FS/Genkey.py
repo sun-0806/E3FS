b21d223cb2eb0957ef27c8b5cee9a8f1f0e07826
sun-0806 <95276015+sun-0806@users.noreply.github.com>
2025-02-13 22:32:51 +0800

    Update Genkey.py

--- E3FS/Genkey.py
+++ E3FS/Genkey.py
@@ -4,6 +4,11 @@ import numpy as np
 
 
 def Keygen(m):
+    '''
+    ASPE Key Generation.
+    :param m: The length of the index
+    :return: ASPE keys
+    '''
     m1 = generate_invertible_matrix(m)
     m2 = generate_invertible_matrix(m)
     S = np.random.randint(0, 2, [m])
@@ -15,6 +20,11 @@ def Keygen(m):
 
 
 def Hash_keys(hash_count):
+    '''
+    PRF Key Generation.
+    :param hash_count: the number of hash functions
+    :return: PRF keys
+    '''
     K = []
     for i in range(hash_count):
         K.append(secrets.token_hex(16).encode())
@@ -22,6 +32,11 @@ def Hash_keys(hash_count):
 
 
 def generate_invertible_matrix(size):
+    '''
+    Generate invertible matrixes
+    :param size: matrix dimension
+    :return: invertible matrixes
+    '''
     while True:
         matrix = np.random.rand(size, size)
 
