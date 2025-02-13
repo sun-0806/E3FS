c8f0a20836cc1f53c16ef9341598a4bfecdef7b0
sun-0806 <95276015+sun-0806@users.noreply.github.com>
2025-02-14 00:06:22 +0800

    Update RSA_signture.py

--- VFSA/RSA_signture.py
+++ VFSA/RSA_signture.py
@@ -3,7 +3,9 @@ import Crypto.Signature.PKCS1_v1_5 as sign_PKCS1_v1_5
 from Crypto import Hash
 
 def to_verify_with_public_key(signature, plain_text):
-
+    '''
+    Public key check algorithm
+    '''
     with open("d.pem", "rb") as x:
         my_public_key = x.read() 
     verifier = sign_PKCS1_v1_5.new(RSA.importKey(my_public_key))
@@ -13,6 +15,9 @@ def to_verify_with_public_key(signature, plain_text):
     return verify 
 
 def to_sign(plain_text):
+    '''
+    Private key signature algorithm
+    '''
     with open("c.pem", "rb") as x:
         my_private_key = x.read() 
     signer_pri_obj = sign_PKCS1_v1_5.new(RSA.importKey(my_private_key))
