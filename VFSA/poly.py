302a975eca676a1a9ade8dd28a0feaaf02b83205
sun-0806 <95276015+sun-0806@users.noreply.github.com>
2025-02-14 00:14:05 +0800

    Update poly.py

--- VFSA/poly.py
+++ VFSA/poly.py
@@ -2,6 +2,7 @@ import numpy as np
 from pypbc import *
 
 def add_poly(L1,L2): 
+    # Polynomial addition, the addition of coefficients of the same degree
     R=[]
     if len(L1)>len(L2):
         L1,L2=L2,L1
@@ -13,32 +14,35 @@ def add_poly(L1,L2):
     return R
 
 def multiply_poly(L1,L2,pairing):
+    # Polynomial multiplication
     if len(L1)>len(L2):
         L1,L2=L2,L1
     zero=[];R=[]
     for i in L1:
-        T=zero[:]
+        T=zero[:] # Store the resulting polynomials produced in the middle and update the list length of the resulting polynomials each time
         for j in L2:
             T.append(i*j)
         R=add_poly(R,T)
-        zero=zero+[Element.zero(pairing, Zr)]
+        zero=zero+[Element.zero(pairing, Zr)] # Each new multiform has a degree 1 higher than the previous polynomial, increasing the length of the list, so fill in an extra 0
     return R
 
 def divide_poly(L1,L2,pairing):
+    # polynomial division
     if len(L1)<len(L2):return 0,L1
     d=len(L1)-len(L2)
     T=L1[:]
     R=[]
     for i in range(d+1):
-        n=T[len(T)-1]/L2[len(L2)-1]
+        n=T[len(T)-1]/L2[len(L2)-1] # The coefficient of the highest order of the dividend divided by the coefficient of the highest order of the divisor
         R=[n]+R
-        T1=[Element.zero(pairing, Zr)]*(d-i)+[n]
+        T1=[Element.zero(pairing, Zr)]*(d-i)+[n] # Complement the resulting number of zeros
         T2=multiply_poly(T1,L2,pairing)
         T=subtract_poly(T,T2)
         T=T[:len(T)-1]
     return R,T
 
 def subtract_poly(L1,L2): 
+    # Polynomial subtraction
     L2=L2[:]
     for i in range(len(L2)):
         L2[i]=-L2[i]
